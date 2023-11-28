0870b088ef6a807f87f5fc650bc8ef81fc00a371
liu.xin <liu.xin@hylink.com>
2023-11-28 11:25:32 +0800

    优化错误处理

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, env, io::{ Error as IOError, ErrorKind }, path::PathBuf, fs};
+use std::{error::Error, env, path::PathBuf, fs};
 
 use walkdir::{WalkDir, DirEntry};
 
@@ -10,7 +10,9 @@ impl Config {
     pub fn new() -> Result<Config, Box<dyn Error>>{
 
         let mut args = env::args();
+
         args.next();
+
         let command = args.next().unwrap_or("".to_string());
 
         let path;
@@ -25,10 +27,10 @@ impl Config {
                 path = args.next().unwrap_or("".to_string());
     
                 if path.is_empty() {
-                    return Err(Box::new(IOError::new(ErrorKind::Other, "请输入AddonPackages路径")));
+                    return Err("请输入AddonPackages路径".into());
                 }
             }else {
-                return Err(Box::new(IOError::new(ErrorKind::Other, format!("{} 没有这个指令 eg. -conf ./AddonPackages", command))));
+                return Err(format!("{} 没有这个指令 eg. -conf ./AddonPackages", command).into());
             }
 
         }
@@ -58,20 +60,20 @@ fn work(path: &str, filename: &str) ->Result<(), Box<dyn Error>> {
     let author = filename.split('.').next().unwrap_or("");
 
     if author.is_empty() {
-        return Err(Box::new(IOError::new(ErrorKind::Other, format!("获取作者名称失败: {}", filename))));
+        return Err(format!("获取作者名称失败: {}", filename).into());
     }
 
     let from = PathBuf::from(path);
     let to = PathBuf::from(format!("{}/{}", author, filename).as_str());
 
     if let Err(e) = fs::create_dir_all(&author) {
-        return Err(Box::new(IOError::new(ErrorKind::Other, format!("创建目录失败: {} {}", to.display(), e))));
+        return Err(format!("创建目录失败: {} {}", to.display(), e).into());
     }
 
     match fs::rename(&from, &to) {
         Ok(_) => println!("移动文件: {} -> {}", from.display(), to.display()),
         Err(e) => {
-            return Err(Box::new(IOError::new(ErrorKind::Other, format!("移动文件失败: {} -> {} {}", from.display(), to.display(), e))));
+            return Err(format!("移动文件失败: {} -> {} {}", from.display(), to.display(), e).into());
         },
     };
 
